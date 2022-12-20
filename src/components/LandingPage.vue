431cc47da2d8c6cd43e1cb8b7205088f13c453cf
Arun Geo Thomas <ArunGeo.Thomas@nl.bosch.com>
2022-12-20 21:15:11 +0100

    Initial frontend page
    A test page containing a v-file-input vue component which
    lets selecting a file.
    
    Signed-off-by: Arun Geo Thoomas <arungeothomas@gmail.com>

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+      <v-row class="text-left">
+        <v-col cols="12">
+            <div>Upload File</div>
+            <v-file-input
+              outlined
+              dense
+              show-size
+              truncate-length="30"
+              v-model="chosenFile"></v-file-input>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+    export default {
+      name: 'LandingPage',
+  
+      data: () => ({
+       chosenFile: null,
+      }),
+    }
+  </script>
+  
